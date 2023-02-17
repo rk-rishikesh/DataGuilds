49f88a8f0368e7fc0339b44ba0feb6ae3fa8af7b
Yogita Sawant <120160846+yogita1003745@users.noreply.github.com>
2023-02-17 14:54:02 +0530

    css changes

--- src/pages/dao-dashboard/dao-dashboard.js
+++ src/pages/dao-dashboard/dao-dashboard.js
@@ -16,7 +16,7 @@ function Dashboard() {
     {
       id: 3,
       uri: "uri 3",
-      status: "Reject",
+      status: "Pending",
     },
     {
       id: 4,
@@ -24,149 +24,68 @@ function Dashboard() {
       status: "Accept",
     },
   ];
+
+  const updateStatus=()=>{
+    alert("called")
+  }
+
   return (
     <>
       <div className="dashboard-data">
-        <h2 style={{ margin: "0px 0px 20px 0px" }}>Dao Dashboard Table</h2>
-        <p>This is dummy loreum text...........</p>
-        <div className="row">
-          <div className="col">
-            <Card style={{ margin: "20px 0px 0px 130px" }}>
-              <Card.Img
-                src="https://www.shutterstock.com/image-vector/simple-infographic-line-chart-sky-260nw-534979027.jpg"
-                width="300px"
-                height="200px"
-              />
-            </Card>
-          </div>
-          <div className="col">
-            <Card style={{ width: "580px", height: " 200px",margin:'19px 0px 0px 30px' }}>
-              <Card.Title style={{padding:'20px',color:'blue'}}>Recent Updates</Card.Title>
-              <Card.Text>count</Card.Text>
-            </Card>
-          </div>
-        </div>
-
-        <Card
-          style={{
-            width: "1250px",
-            backgroundColor: "white",
-            alignItems: "center",
-            height: "250px",
-            margin: "40px 0px 0px 130px",
-          }}
-        >
-          <h2
-            style={{
-              margin: "20px 1000px 0px 0px",
-              fontSize: "20px",
-              color: "blue",
-            }}
-          >
-            Recent Data
-          </h2>
-          <table style={{ width: "90%", margin: "20px 0px 0px 0px" }}>
-            <thead>
-              <tr>
-                <th>Token_ID</th>
-                <th>Token_URI</th>
-                <th>Praposal_Status</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {dashboardData.map((data, i) => {
-                return (
-                  <>
-                    <tr>
-                      <td key={i}>{data.id}</td>
-                      <td>{data.uri}</td>
-                      <td>{data.status}</td>
-                      {/* <td>
-                    <Button
-                      style={{
-                        backgroundColor: "black",
-                        border: "none",
-                        borderRadius: "0px",
-                        fontSize: "10px",
-                        margin: "0px 0px 0px 0px",
-                        width: "80px",
-                      }}
-                    >
-                      UpVote
-                    </Button>
-                    &nbsp;&nbsp;&nbsp;
-                    <Button
-                      style={{
-                        backgroundColor: "black",
-                        border: "none",
-                        borderRadius: "0px",
-                        margin: "0px 0px 0px 0px",
-                        fontSize: "10px",
-                        width: "80px",
-                      }}
-                    >
-                      DownVote
-                    </Button>
-                  </td> */}
-                    </tr>
-                  </>
-                );
-              })}
-            </tbody>
-          </table>
-        </Card>
+        <table>
+          <thead>
+            <tr>
+              <th>Token_ID</th>
+              <th>Token_URI</th>
+              <th>Praposal_Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dashboardData.map((data, i) => {
+              return (
+                <>
+                  <tr>
+                    <td key={i}>{data.id}</td>
+                    <td>{data.uri}</td>
+                    <td>{data.status}</td>
+                    <td>
+                      {data.status === "Accept" && (
+                        <img
+                          src="https://t3.ftcdn.net/jpg/01/57/86/44/360_F_157864480_TFm1nQsUI1o8VLKg6SK6yV9P6tsK4TXN.jpg"
+                          width="50"
+                          onClick={updateStatus}
+                        />
+                      )}
+                      {data.status === "Reject" && (
+                        <img
+                          src="https://img.freepik.com/premium-vector/red-cross-mark-icon-negative-choice-symbol-sign-app-button_744955-339.jpg?w=360"
+                          width="35"
+                          onClick={updateStatus}
+                        />
+                      )}
+                      {data.status === "Pending" && (
+                        <>
+                          <img
+                            src="https://t3.ftcdn.net/jpg/01/57/86/44/360_F_157864480_TFm1nQsUI1o8VLKg6SK6yV9P6tsK4TXN.jpg"
+                            width="50"
+                            onClick={updateStatus}
+                          />
+                          <img
+                            src="https://img.freepik.com/premium-vector/red-cross-mark-icon-negative-choice-symbol-sign-app-button_744955-339.jpg?w=360"
+                            width="35"
+                            onClick={updateStatus}
+                          />
+                        </>
+                      )}
+                    </td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
-      {/* <table>
-        <thead>
-          <tr>
-            <th>Token_ID</th>
-            <th>Token_URI</th>
-            <th>Praposal_Status</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {dashboardData.map((data, i) => {
-            return (
-              <>
-                <tr>
-                  <td key={i}>{data.id}</td>
-                  <td>{data.uri}</td>
-                  <td>{data.status}</td>
-                  <td>
-                    <Button
-                      style={{
-                        backgroundColor: "black",
-                        border: "none",
-                        borderRadius: "0px",
-                        fontSize: "10px",
-                        margin: "0px 0px 0px 0px",
-                        width: "80px",
-                      }}
-                    >
-                      UpVote
-                    </Button>
-                    &nbsp;&nbsp;&nbsp;
-                    <Button
-                      style={{
-                        backgroundColor: "black",
-                        border: "none",
-                        borderRadius: "0px",
-                        margin: "0px 0px 0px 0px",
-                        fontSize: "10px",
-                        width: "80px",
-                      }}
-                    >
-                      DownVote
-                    </Button>
-                  </td>
-                </tr>
-              </>
-            );
-          })}
-        </tbody>
-      </table> */}
     </>
   );
 }
